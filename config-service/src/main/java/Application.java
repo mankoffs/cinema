ececc0efb0317d7ddf48e6b8c7b2a6b7a132fd7c
smanko <smanko@playtika.com>
2017-10-20 22:58:52 +0300

    initial commit

---
+++ config-service/src/main/java/Application.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by smanko on 10/20/2017.
+ */
+
+@Configuration
+@EnableAutoConfiguration
+public class Application {
+
+
+}
