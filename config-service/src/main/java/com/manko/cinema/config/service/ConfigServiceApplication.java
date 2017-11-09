afea868e5228f4c032ec12cacd63a9d23f394a07
smanko <smanko@playtika.com>
2017-11-09 17:17:32 +0200

    init several tables db

--- config-service/src/main/java/com/manko/cinema/config/service/ConfigServiceApplication.java
+++ config-service/src/main/java/com/manko/cinema/config/service/ConfigServiceApplication.java
@@ -1,15 +1,19 @@
 package com.manko.cinema.config.service;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by smanko on 10/20/2017.
  */
 
+@Configuration
+@EnableAutoConfiguration
+@EnableDiscoveryClient
 @EnableConfigServer
-@SpringBootApplication
 public class ConfigServiceApplication {
 
     public static void main(String[] args) {
---
+++ movie-service/src/main/java/сom/manko/cinema/movie/service/MovieApplication.java
@@ -0,0 +1,11 @@
+package сom.manko.cinema.movie.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@SpringCloudApplication
+public class MovieApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MovieApplication.class);
+    }
+}
