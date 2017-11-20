272ab2b13f6bcc5434f693ec222d148fa44b2f6b
smanko <smanko@playtika.com>
2017-11-20 15:26:48 +0200

    added discovery service (eureka)

---
+++ discovery-service/src/main/java/сom/manko/cinema/discovery/service/EurekaApplication.java
@@ -0,0 +1,15 @@
+package сom.manko.cinema.discovery.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class);
+    }
+}
--- movie-service/src/main/java/сom/manko/cinema/movie/service/MovieApplication.java
+++ movie-service/src/main/java/сom/manko/cinema/movie/service/MovieApplication.java
@@ -1,9 +1,13 @@
 package сom.manko.cinema.movie.service;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringCloudApplication
+@EnableDiscoveryClient
+@EnableAutoConfiguration
 public class MovieApplication {
     public static void main(String[] args) {
         SpringApplication.run(MovieApplication.class);
