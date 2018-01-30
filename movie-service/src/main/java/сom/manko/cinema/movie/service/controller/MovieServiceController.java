f04fb0d59fcd5aa2c830f0f4714c601ca8e7d5f9
smanko <smanko@playtika.com>
2018-01-30 20:12:56 +0200

    implement movie service controller (/getMovie endpoint)

--- movie-service/src/main/java/сom/manko/cinema/movie/service/controller/MovieServiceController.java
+++ movie-service/src/main/java/сom/manko/cinema/movie/service/controller/MovieServiceController.java
@@ -1,25 +1,39 @@
 package сom.manko.cinema.movie.service.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import сom.manko.cinema.movie.service.model.Movie;
-import сom.manko.cinema.movie.service.persistent.MovieRepository;
+import сom.manko.cinema.movie.service.domain.MovieEntity;
+import сom.manko.cinema.movie.service.dto.MovieInfo;
+import сom.manko.cinema.movie.service.manager.MovieManager;
+
+import java.util.List;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 
 @RestController
+@RequestMapping("/movie")
+@Slf4j
 public class MovieServiceController {
 
     @Autowired
-    private MovieRepository movieRepository;
+    private MovieManager movieManager;
 
-    @GetMapping(value = "/movie")
-    public Movie getMovie(@RequestParam long movieId) {
-        return movieRepository.findOne(movieId);
+    @GetMapping(value = "/getMovie", produces = APPLICATION_JSON_UTF8_VALUE)
+    public MovieInfo getMovie(@RequestParam Long movieId) {
+        log.info("Get movie by movieId:{}", movieId);
+        MovieInfo movie = movieManager.getMovie(movieId);
+        log.info("Result for movieId:{}, result:{}", movieId, movie);
+        return movie;
     }
 
-    @GetMapping(value = "/movieAll")
-    public Iterable<Movie> getAllMovie() {
-        return movieRepository.findAll();
+    @GetMapping(value = "/getMovies")
+    public List<MovieEntity> getMovies() {
+        log.info("Get all movies");
+        return movieManager.getMovies();
+
     }
 }
--- movie-service/src/main/java/сom/manko/cinema/movie/service/persistent/MovieRepository.java
+++ movie-service/src/main/java/сom/manko/cinema/movie/service/persistent/MovieRepository.java
@@ -1,7 +1,7 @@
 package сom.manko.cinema.movie.service.persistent;
 
 import org.springframework.data.repository.CrudRepository;
-import сom.manko.cinema.movie.service.model.Movie;
+import сom.manko.cinema.movie.service.domain.MovieEntity;
 
-public interface MovieRepository extends CrudRepository<Movie, Long> {
+public interface MovieRepository extends CrudRepository<MovieEntity, Long> {
 }
